53ef9ddbd698bc2547d84a62f82de45fa9c07784
vishaalagartha <vishaalagartha@gmail.com>
2020-07-04 18:56:27 -0700

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,46 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="_8a_scraper",
+    version="0.0.1",
+    author="Vishaal Agartha",
+    author_email="vishaalagartha@gmail.com",
+    license="MIT",
+    description="A Python client for scraping data from 8a.nu",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vishaalagartha/8a_scraper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        'beautifulsoup4==4.9.1',
+        'bs4==0.0.1',
+        'certifi==2020.6.20',
+        'chardet==3.0.4',
+        'idna==2.10',
+        'requests==2.24.0',
+        'selenium==3.141.0',
+        'soupsieve==2.0.1',
+        'urllib3==1.25.9'
+    ],
+    extras_require={
+        'test': ['unittest'],
+    },
+    keywords=[
+        "climbing",
+        "rockclimbing",
+        "bouldering",
+        "sportclimbing",
+        "8a",
+        "8a.nu",
+        "data mining",
+        ],
+)
