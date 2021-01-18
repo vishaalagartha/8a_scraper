68a7aa1cfb1b2287a36f0b46566031fd38a0ea0b
ItaiAxelrad <itaiaxelrad@gmail.com>
2021-01-18 16:44:54 -0700

    fix driver login

--- _8a_scraper/utils.py
+++ _8a_scraper/utils.py
@@ -7,7 +7,6 @@ def login():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument(
         '--disable-blink-features=AutomationControlled')
     chrome_options.add_argument("window-size=1280,800")
